d80ed3e3b03e652ddaf2c1258d9f1165d05a0c9c
kishik <kishik.maxim@gmail.com>
2023-10-23 02:02:55 +0300

    research started

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ import numpy as np
 
 data = pd.read_csv('b631bf23-204c-4ed9-b55d-0499a8087859_Data.csv', encoding='cp1251')
 data2 = pd.read_csv('b631bf23-204c-4ed9-b55d-0499a8087859_Series - Metadata.csv', encoding='cp1251')
-
+data3 = pd.read_csv('rankings.csv', encoding='cp1251')
 st.dataframe(data=data)
 st.dataframe(data=data2)
+st.dataframe(data=data3)
+
+# https://archive.doingbusiness.org/en/methodology
